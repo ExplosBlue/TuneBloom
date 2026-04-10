70b903ea15510a6c31338b6dd2de1e95996c8182
STUPID Modder <stupidmoddersoup@gmail.com>
2026-04-10 03:19:27 -0300

    add assertion msg box

--- TuneBloom/src/main.cpp
+++ TuneBloom/src/main.cpp
@@ -1,3 +1,4 @@
+#include <devenv/seadAssertConfig.h>
 #include <heap/seadExpHeap.h>
 #include <heap/seadHeapMgr.h>
 
@@ -6,8 +7,19 @@
 
 #include "Utilll.h"
 
+void AssertException(const char* msg)
+{
+#if defined(SEAD_PLATFORM_WINDOWS)
+    sead::FormatFixedSafeString<2048> info("%s\nAn unexpected error has ocurred, please report this issue on 'https://github.com/stupidestmodder/TuneBloom/issues' or ask here 'https://go.nsmbu.net/discord'", msg);
+    MessageBoxA(nullptr, info.cstr(), "ASSERTION ERROR", 0);
+#endif // SEAD_PLATFORM_WINDOWS
+}
+
 int main()
 {
+    sead::Delegate1 deleg = sead::FunctionDelegateCreator<const char*>(&AssertException);
+    sead::AssertConfig::registerFinalCallback(&deleg);
+
     sead::Framework::InitializeArg initArg;
     initArg.heap_size = 100 * 1024 * 1024; // 100 MB
     AppFramework::initialize(initArg);
@@ -41,7 +53,7 @@ int main()
     delete framework;
 }
 
-#ifdef SEAD_PLATFORM_WINDOWS
+#if defined(SEAD_PLATFORM_WINDOWS)
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow)
 {
     main();

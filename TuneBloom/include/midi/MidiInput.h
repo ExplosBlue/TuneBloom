7c25fa919b0ae59a7ba3bfce7a5b69191786c6f6
Gimzie <gimzie@outlook.com>
2026-06-10 22:21:09 -0500

    i hate windows

--- TuneBloom/include/midi/MidiInput.h
+++ TuneBloom/include/midi/MidiInput.h
@@ -15,13 +15,13 @@ public:
     void poll();
     bool isRunning() const;
 
-private:
     struct QueuedEvent { s32 msg; s32 key; f32 vel; };
     static const s32 kQueueSize = 256;
     volatile s32 mQueueRead = 0;
     volatile s32 mQueueWrite = 0;
     QueuedEvent mQueue[kQueueSize];
 
+private:
     Callback mCallback = nullptr;
     void* mUserData = nullptr;
 #if defined(SEAD_PLATFORM_LINUX)
